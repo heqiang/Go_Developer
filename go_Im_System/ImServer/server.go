9c0a5c5af3962044758afdfb0093c1df83781284
hq <1422127065@qq.com>
2021-08-28 16:51:21 +0800

    Add: 群聊广播功能完成

--- go_Im_System/ImServer/server.go
+++ go_Im_System/ImServer/server.go
@@ -3,91 +3,103 @@ package ImServer
 import (
 	"Go_Developer/go_Im_System/Imuser"
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
 
-type  Server struct {
-	IP string
-	Port  int
+type Server struct {
+	IP   string
+	Port int
 	// 在线用户的列表
-	OnlineMap  map[string]*Imuser.User
-	MpLock  sync.RWMutex
+	OnlineMap map[string]*Imuser.User
+	MpLock    sync.RWMutex
 
 	//消息广播channel
-	Message  chan string
+	Message chan string
 }
 
-func NewServer(ip string,port int) *Server {
+func NewServer(ip string, port int) *Server {
 
-	return  &Server{
-		 IP: ip,
-		 Port: port,
-		 OnlineMap: make(map[string]*Imuser.User),
-		 Message : make(chan string),
+	return &Server{
+		IP:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*Imuser.User),
+		Message:   make(chan string),
 	}
 }
 
 // Handle 处理连接的业务
-func  (server *Server) Handle(conn net.Conn) {
-	fmt.Println("连接成功")
+func (server *Server) Handle(conn net.Conn) {
+	fmt.Println(conn.RemoteAddr().String() + "连接成功")
 	// 用户上线 将用户加入 onlinemap中
-	user:=Imuser.NewUser(conn)
+	user := Imuser.NewUser(conn)
 	server.MpLock.Lock()
 	server.OnlineMap[user.Name] = user
 	server.MpLock.Unlock()
+	server.BroadCast(user, "上线了")
+	go func() {
+		byteValus := make([]byte, 4096)
+		for {
+			n, err := conn.Read(byteValus)
+			if err != nil && err != io.EOF {
+				fmt.Println("conn read err:", err)
+			}
+			if n == 0 {
+				server.BroadCast(user, "下线")
+				return
+			}
+			msg := string(byteValus[:n])
+			server.BroadCast(user, msg)
+		}
+	}()
 
-	server.BroadCast(user,"上线了")
-
-	select {
-
-	}
+	//当前handler阻塞 使 协程不能死亡
+	select {}
 
 }
-func  (server *Server)ListenMesage()  {
+func (server *Server) ListenMesage() {
 	for {
-		msg:=<-server.Message
+		msg := <-server.Message
 		server.MpLock.Lock()
-		for _,cli:=range  server.OnlineMap{
+		for _, cli := range server.OnlineMap {
 			cli.C <- msg
 		}
-
+		server.MpLock.Unlock()
 	}
 }
 
-
 // BroadCast 消息广播
-func  (server *Server)BroadCast(user *Imuser.User,msg string)  {
-		sendMsg := "[" + user.Addr+"]"+user.Name+":" +msg
-		// 想Message 中发送消息  所以应该有一个监听MessAge的方法
-		server.Message <- sendMsg
+func (server *Server) BroadCast(user *Imuser.User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	fmt.Println(sendMsg)
+	// 想Message 中发送消息  所以应该有一个监听MessAge的方法
+	server.Message <- sendMsg
 }
 
 // Start 服务端连接
-func  (server Server)Start()  {
+func (server Server) Start() {
 	// Socket Listen
-	liesten,err:=net.Listen("tcp",fmt.Sprintf("%s:%d",server.IP,server.Port))
-	if err!=nil{
-		fmt.Println("net Listen err",err)
+	liesten, err := net.Listen("tcp", fmt.Sprintf("%s:%d", server.IP, server.Port))
+	if err != nil {
+		fmt.Println("net Listen err", err)
 		return
 	}
-	defer  liesten.Close()
+	defer liesten.Close()
 	fmt.Println("监听开始")
 	// 启动监听ListenMessage
 	go server.ListenMesage()
 	for {
 		//Accept
-		conn,err:= liesten.Accept()
-		if err!=nil{
-			fmt.Println("Listen accept err",err)
+		conn, err := liesten.Accept()
+		if err != nil {
+			fmt.Println("Listen accept err", err)
 			continue
 		}
 		defer conn.Close()
 		// do handler
-		fmt.Println("")
 		go server.Handle(conn)
 	}
-
 
 	//close Listen socket
 }
